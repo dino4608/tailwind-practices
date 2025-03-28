49ea5481f9068ae71b340f69eea811f2b6a9bcf7
tn9th8 <tn9th8@gmail.com>
2025-03-28 15:15:09 +0700

    box

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,13 @@ const App = () => {
 
   return (
     <>
-      <div className="columns-2">
-        <p>Well, let me tell you something, ...</p>
-        <p className="break-after-column">Sure, go ahead, laugh...</p>
-        <p>Maybe we can live without...</p>
-        <p>Look. If you think this is...</p>
-      </div>
-
-      <div className="columns-2">
-        <p>Well, let me tell you something, ...</p>
-        <p className="break-before-column md:break-before-auto">Sure, go ahead, laugh...</p>
-        <p>Maybe we can live without...</p>
-        <p>Look. If you think this is...</p>
-      </div>
-
-      <div className="columns-2">
-        <p>Well, let me tell you something, ...</p>
-        <p className="break-inside-avoid-column">Sure, go ahead, laugh...</p>
-        <p>Maybe we can live without...</p>
-        <p>Look. If you think this is...</p>
-      </div>
+      <span className="inline-block bg-gradient-to-r from-indigo-600 to-pink-500 px-2 text-white">
+        Hello<br />World
+      </span>
+      <br />
+      <span className="box-decoration-clone bg-linear-to-r from-indigo-600 to-pink-500 px-2 text-white ...">
+        Hello<br />World
+      </span>
     </>
   )
 }
