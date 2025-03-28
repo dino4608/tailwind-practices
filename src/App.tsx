5ceb3f74ddfe0089e79551f7d5ed94862b8b08af
tn9th8 <tn9th8@gmail.com>
2025-03-28 15:07:09 +0700

    break

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,25 @@ const App = () => {
 
   return (
     <>
-      <div className="columns-3 gap-16">
-        <img className="aspect-3/2" src="ratio-3x2.jpg" />
-        <img className="aspect-square" src="ratio-3x2.jpg" />
-        <img className="aspect-video" src="ratio-3x2.jpg" />
-
-        <img className="aspect-3/2" src="ratio-3x2.jpg" />
-        <img className="aspect-square" src="ratio-3x2.jpg" />
-        <img className="aspect-video" src="ratio-3x2.jpg" />
-
-        <img className="aspect-3/2" src="ratio-3x2.jpg" />
-        <img className="aspect-square" src="ratio-3x2.jpg" />
-        <img className="aspect-video" src="ratio-3x2.jpg" />
-
+      <div className="columns-2">
+        <p>Well, let me tell you something, ...</p>
+        <p className="break-after-column">Sure, go ahead, laugh...</p>
+        <p>Maybe we can live without...</p>
+        <p>Look. If you think this is...</p>
       </div>
-      <div className="columns-2 gap-8 sm:columns-4 sm:gap-4">
-        <img className="aspect-3/2" src="ratio-3x2.jpg" />
-        <img className="aspect-square ..." src="ratio-3x2.jpg" />
-        <img className="aspect-square ..." src="ratio-3x2.jpg" />
-        <img className="aspect-3/2 ..." src="ratio-3x2.jpg" />
+
+      <div className="columns-2">
+        <p>Well, let me tell you something, ...</p>
+        <p className="break-before-column md:break-before-auto">Sure, go ahead, laugh...</p>
+        <p>Maybe we can live without...</p>
+        <p>Look. If you think this is...</p>
+      </div>
+
+      <div className="columns-2">
+        <p>Well, let me tell you something, ...</p>
+        <p className="break-inside-avoid-column">Sure, go ahead, laugh...</p>
+        <p>Maybe we can live without...</p>
+        <p>Look. If you think this is...</p>
       </div>
     </>
   )
