779f1b0a7eb9d6a5a134f859ba30c0914639ecbd
tn9th8 <tn9th8@gmail.com>
2025-03-28 14:43:59 +0700

    columns

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,26 @@ const App = () => {
 
   return (
     <>
-      <iframe className="aspect-video w-full md:aspect-square md:w-100" src="https://www.youtube.com/embed/dQw4w9WgXcQ"></iframe>
+      <div className="columns-3 gap-16">
+        <img className="aspect-3/2" src="ratio-3x2.jpg" />
+        <img className="aspect-square" src="ratio-3x2.jpg" />
+        <img className="aspect-video" src="ratio-3x2.jpg" />
+
+        <img className="aspect-3/2" src="ratio-3x2.jpg" />
+        <img className="aspect-square" src="ratio-3x2.jpg" />
+        <img className="aspect-video" src="ratio-3x2.jpg" />
+
+        <img className="aspect-3/2" src="ratio-3x2.jpg" />
+        <img className="aspect-square" src="ratio-3x2.jpg" />
+        <img className="aspect-video" src="ratio-3x2.jpg" />
+
+      </div>
+      <div className="columns-2 gap-8 sm:columns-4 sm:gap-4">
+        <img className="aspect-3/2" src="ratio-3x2.jpg" />
+        <img className="aspect-square ..." src="ratio-3x2.jpg" />
+        <img className="aspect-square ..." src="ratio-3x2.jpg" />
+        <img className="aspect-3/2 ..." src="ratio-3x2.jpg" />
+      </div>
     </>
   )
 }
