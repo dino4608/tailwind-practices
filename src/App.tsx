cc105775e1f09b083e8bfae21ce909ca68c8045c
tn9th8 <tn9th8@gmail.com>
2025-03-28 14:26:07 +0700

    aspect ratio

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,7 @@ const App = () => {
 
   return (
     <>
-      <div className="flex items-center gap-4 rounded-lg bg-white p-6 shadow-md outline outline-black/5 dark:bg-gray-800">
-        <span className="inline-flex shrink-0 rounded-full border border-pink-300 bg-pink-100 p-2 dark:border-pink-300/10 dark:bg-pink-400/10">
-          <svg className="size-6 stroke-pink-700 dark:stroke-pink-500">O</svg>
-        </span>
-        <div>
-          <p className="text-gray-700 dark:text-gray-400">
-            <span className="font-medium text-gray-950 dark:text-white">Tom Watson </span>
-            mentioned you in
-            <span className="font-medium text-gray-950 dark:text-white"> Logo redesign</span>
-          </p>
-          <time className="mt-1 block text-gray-500" dateTime="9:37">9:37am</time>
-        </div>
-      </div>
+      <iframe className="aspect-video w-full md:aspect-square md:w-100" src="https://www.youtube.com/embed/dQw4w9WgXcQ"></iframe>
     </>
   )
 }
